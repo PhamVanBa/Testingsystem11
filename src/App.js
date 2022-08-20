bc87f2b2c85f900baa3d90a2acdff71eb2dc410f
PHAM VAN BA <bale.mip@gmail.com>
2022-08-20 19:17:33 +0900

    createDeleteup

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import WithLoading from "./hoc/withLoading";
 import { connect } from "react-redux";
 import Home from "./components/Home";
 import { Switch, Route, Link, Redirect } from "react-router-dom";
+import CreateGroup from "./components/group/CreateGroup";
+import DeleteGroup from "./components/group/DeleteGroup";
 
 function App(props) {
   const getListGroup = props.getListGroupAsyncAction;
@@ -29,6 +31,12 @@ function App(props) {
           <li>
             <Link to="/groups">Group List</Link>
           </li>
+          <li>
+            <Link to="/creategroup">Create Group</Link>
+          </li>
+          <li>
+            <Link to="/deletegroup">Delete Group</Link>
+          </li>
         </ul>
       </nav>
 
@@ -44,6 +52,8 @@ function App(props) {
           )}
           exact
         />
+        <Route path="/creategroup" component={CreateGroup} exact />
+        <Route path="/deletegroup" component={DeleteGroup} exact />
         <Redirect to="/" />
       </Switch>
     </div>
